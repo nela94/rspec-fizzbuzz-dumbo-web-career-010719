b781dd4857e413d913ddb96f1f4efdc769e28e29
Manuela  Sanchez <m.sanchez947@hotmail.com>
2018-12-23 12:22:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
-def fizzbuzz
-
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "Buzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  else
+end
 end
 
 
